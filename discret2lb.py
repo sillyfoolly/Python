f20caccbd8cff16062cf87ca534cde3f5c507a96
sillyfoolly <andrey.gromyko.97@gmail.com>
2024-02-26 14:20:58 +0300

    govno

--- discret2lb.py
+++ discret2lb.py
@@ -8,10 +8,11 @@ def copy(m):
     for i in range(4):
         for j in range(4):
           new[i][j] = m[i][j]
+
     return new
 
 #   Нахождение транзитивного замыкания первым способом
-def FirstMethod(m):
+def firstMethod(m):
     trn1 = copy(m)
     trn2 = copy(m)
 
@@ -32,7 +33,7 @@ def FirstMethod(m):
     if trn2 == trn1:
         print('\nМатрица транзитивного замыкания:')
         for i in range(4): print(trn2[i])
-    else : FirstMethod(trn2)
+    else : firstMethod(trn2)
 
 #   Нахождение транзитивного замыкания вторым способом
 def SecondMethod(m):
@@ -43,15 +44,21 @@ def SecondMethod(m):
             if i != j and trn[i][j] == 1:
                 for k in range(4):
                     trn[i][k] = trn[i][k] + trn[j][k]
+
     for i in range(4):
         for j in range(4):
             if trn[i][j] > 1:
                 trn[i][j] = 1               
+
     print('\nМатрица транзитивного замыкания:')
+
     for i in range(4): 
         print(trn[i])
         pwr += sum(trn[i])
+
     return pwr
+
+
 pwr = 0
 matrix = [
     [0, 1, 1, 1],
@@ -87,7 +94,7 @@ for i in range(4):
     pwr += sum(sym[i])
 
 #   Вызов функций для нахождения транзитивного замыкания
-FirstMethod(matrix)
+firstMethod(matrix)
 pwr += SecondMethod(matrix)
 
 print('\nСумма мощностей четырех матриц: ',pwr)
