099487635fabe8303e6df0259c347ead04db11a8
sillyfoolly <andrey.gromyko.97@gmail.com>
2024-02-28 01:45:43 +0300

    ,,,,,,

--- discret5.py
+++ discret5.py
@@ -1,14 +1,22 @@
-mass = [
-    [0, 21, 16, 0, 0, 0, 22],   
-    [21, 0, 15, 0, 0, 0, 0],    
-    [16, 15, 0, 27, 13, 0, 17], 
-    [0, 0, 27, 0, 19, 0, 0],    
-    [0, 0, 13, 19, 0, 24, 0],   
-    [0, 0, 0, 0, 24, 0, 29],    
-    [22, 0, 17, 0, 0, 29, 0],   
-    ]
+def prima():
+    n = 7
+    used = [1, 0, 1, 0, 0, 0, 0]
+    summ = 16
+    a, b = 0, 0
+    for k in range(5):
+        min = 99
+        for i in range(n):
+            if used[i]:
+                for j in range(n):
+                    if used[j] == 0:
+                        if mass[i][j] < min and mass[i][j] != 0:
+                            min = mass[i][j]
+                            a, b = i, j
+        used[b] = 1
+        summ += mass[a][b]
+    print(summ)
 
-
+    
 def kruskal():
     n = 7
     used = [0, 0, 0, 0, 0, 0, 0]
@@ -27,23 +35,16 @@ def kruskal():
 
     print(summ)
 
-def prima():
-    n = 7
-    used = [1, 0, 1, 0, 0, 0, 0]
-    summ = 16
-    a, b = 0, 0
-    for k in range(5):
-        min = 99
-        for i in range(n):
-            if used[i]:
-                for j in range(n):
-                    if used[j] == 0:
-                        if mass[i][j] < min and mass[i][j] != 0:
-                            min = mass[i][j]
-                            a, b = i, j
-        used[b] = 1
-        summ += mass[a][b]
-    print(summ)
+
+mass = [
+    [0, 21, 16, 0, 0, 0, 22],   
+    [21, 0, 15, 0, 0, 0, 0],    
+    [16, 15, 0, 27, 13, 0, 17], 
+    [0, 0, 27, 0, 19, 0, 0],    
+    [0, 0, 13, 19, 0, 24, 0],   
+    [0, 0, 0, 0, 24, 0, 29],    
+    [22, 0, 17, 0, 0, 29, 0],   
+    ]
 
 kruskal()
 prima()
