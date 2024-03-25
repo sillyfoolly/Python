3b74fe414b7594b118d1b5f6fbfce77a0f3c30d8
sillyfoolly <andrey.gromyko.97@gmail.com>
2024-03-25 22:58:57 +0300

    ////

--- DS2.py
+++ DS2.py
@@ -1,3 +1,5 @@
+from math import *
+
 def linear_congruent_method(x, n):
     mass = [x/(pow(2, 31))]
 
@@ -7,13 +9,10 @@ def linear_congruent_method(x, n):
 
     return mass
 
-def equal_spread(mass):
-    newmass = [] 
+def task1(mass):
+    newmass = [-4.5 + (8.5 + 4.5) *  i for i in mass] 
     interval = [0] * 10
     x2 = 0
-
-    for i in mass:
-        newmass.append(-4.5 + (8.5 + 4.5) *  i)
 
     for i in range(10):
         for j in newmass:
@@ -21,20 +20,91 @@ def equal_spread(mass):
                 interval[i] += 1
 
     for i in range(10):
-        x2 = x2 + pow(interval[i] - 2, 2) / 2
+        x2 += pow(interval[i] - 2, 2) / 2
 
-    if x2 < 16.9: print(f'\nB) Последовательность равномерно распределенна ({x2:.1f} < 16.9)')
+    print(f'\nПолученные числа на отрезке [-4.5, 8.5]:\n{newmass}')
 
-    else: print(f'\nB) Последовательность распределенна не равномерно  ({x2:.1f} > 16.9)')
+    if x2 < 16.9: print(f'\nПоследовательность равномерно распределенна ({x2:.1f} < 16.9)')
+
+    else: print(f'\nПоследовательность распределенна не равномерно  ({x2:.1f} > 16.9)')
 
     print(interval)
+
+def task2(mass):
+    # Первый тест
+    newmass = [-4260 * log(i) for i in mass]
+    c = 0
+
+    for i in newmass:
+        if i < 5000:
+            c += 1 
+    mid = c
+    
+    print(f'\n1-ый тест: время работ\n{newmass}\nИз них потребуют перезагрузку {c}')
+    
+    # Второй тест
+    newmass = [-4260 * log(i) for i in mass]
+    c = 0
+
+    for i in newmass:
+        if i < 5000:
+            c += 1 
+    mid += c
+
+    print(f'\n2-ой тест: время работ\n{newmass}\nИз них потребуют перезагрузку {c}')
+
+    # Третий тест
+    newmass = [-4260 * log(i) for i in mass]
+    c = 0
+
+    for i in newmass:
+        if i < 5000:
+            c += 1 
+    mid += c
+    
+    print(f'\n3-ий тест: время работ\n{newmass}\nИз них потребуют перезагрузку {c}')
+    print(f'\nВ среднем потребуют перезагрузку: {mid/3}')
+
+def task3(mass):
+    newmass = []
+    c = 0
+
+    for i in range(0, len(mass)-1, 2):
+        newmass.append((8 + 0.8 * (pow((-2 * log(mass[i])), 1/2) * cos(2*pi*mass[i+1]))))
+        newmass.append((8 + 0.8 * (pow((-2 * log(mass[i])), 1/2) * sin(2*pi*mass[i+1]))))
+
+    for i in newmass:
+        if (6.4 < i < 9.6) == False:
+            c += 1
+
+    print(f'\nСредний диаметр = 8 мм\nсреднее квадратичное отклонение = 0.8')
+    print(newmass, c)
+    
+def task4(mass):
+    newmass = []
+    x = 0; n = 0; c = 0
+
+    for i in mass:
+        x += i - 6
+        n += 1
+        if n == 12:
+            newmass.append(100 + 10 * x)
+            c += 1
+            x = 0
+            n = 0
+
+    print(newmass, c)
 
 
 def main():
 
-    print('\n\n---------- Линейный конгруэнтный метод ----------')
-    mass1 = linear_congruent_method(1080762024, 20)
-    equal_spread(mass1)
+    # mass1 = linear_congruent_method(1080762024, 20)
+    # task1(mass1)
+    # task2(mass1)
+    # mass2 = linear_congruent_method(1080762024, 100)
+    # task3(mass2) 
+    mass3 = linear_congruent_method(1080762024, 4380)
+    task4(mass3)
 
 if __name__ == "__main__":
     main()
